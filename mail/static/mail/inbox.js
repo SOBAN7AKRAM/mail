293ca2906a468251c761103649dc2daf8f15b408
M Soban Akram <chsobanakram@gmail.com>
2024-03-30 09:24:34 +0500

    Almost completed

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose').addEventListener('click',() => compose_email());
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
+function compose_email(r='', s='', b='') {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -18,11 +18,10 @@ function compose_email() {
   document.querySelector('#view-email').style.display = 'none';
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-recipients').value = r;
+  document.querySelector('#compose-subject').value = s;
+  document.querySelector('#compose-body').value = b;
   // Get value
-    
   
 
 
@@ -76,23 +75,11 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   getEmails(mailbox);
-  let conts = document.querySelectorAll('.emailContainer')
-      conts.forEach((cont) => {
-        cont.addEventListener('mouseenter', () => {
-          cont.remove('timestamp');
-          cont.classList.add('showImage');
-        })
-        cont.addEventListener('mouseleave', () => {
-          div3.innerHTML = element.timestamp;
-          div3.classList.remove('showImage');
-        })
-      })
 }
 let getEmails = (mailbox) => {
   fetch(`emails/${mailbox}`)
   .then(response => response.json())
   .then(data => {
-
     data.forEach(element => {
       const div = document.createElement('div');
       const div1 = document.createElement('div');
@@ -119,21 +106,33 @@ let getEmails = (mailbox) => {
       }
       document.querySelector('#emails-view').append(div);
 
-      div.addEventListener('click', () => {
-        getEmail(element.id);
-        markUnread(element.id);
-      })
-      div.addEventListener('mouseenter', () => {
-        let img = document.createElement('div');
-        img.classList.add('imgContainer');
-        div3.append(img);
-        div3.classList.add('subContainer');
-      })
-      div.addEventListener('mouseleave', () => {
-        let img = document.querySelector('.imgContainer');
-        div3.removeChild(img);
-      })
-    });
+      
+      if (mailbox !== 'sent')
+      {
+        div.addEventListener('mouseenter', () => {
+          let img = document.createElement('div');
+          img.classList.add('imgContainer');
+          div3.append(img);
+          div3.classList.add('subContainer');
+          img.addEventListener('click', (event) => {
+            event.stopPropagation();
+            markArchive(element).then(()=> {
+              load_mailbox(mailbox);
+            })
+            
+          })
+        })
+        div.addEventListener('mouseleave', () => {
+          let img = document.querySelector('.imgContainer');
+          div3.removeChild(img);
+        })
+      }
+        div.addEventListener('click', () => {
+          getEmail(element.id);
+          markUnread(element.id);
+        })
+      
+      });
   })
 }
 
@@ -147,6 +146,14 @@ let getEmail = (id) => {
       .catch(err => {
         console.log(err);
       })
+}
+let markArchive = (email) => {
+  return fetch(`/emails/${email.id}`, {
+    method:'PUT',
+    body: JSON.stringify({
+      archived: (email.archived) ? false : true
+    })
+  })
 }
 let markUnread = (id) => {
   fetch(`/emails/${id}`, {
@@ -163,17 +170,31 @@ let viewEmail = (email) => {
       let subject = document.createElement('h4');
       let body = document.createElement('p');
       let timestamp = document.createElement('div');
+      let btn = document.createElement('button');
+      btn.classList.add('btn','btn-sm','btn-outline-primary');
+      btn.type = 'button';
+      btn.textContent = 'Reply';
+      btn.setAttribute('id', 'reply');
       divContainer.classList.add('divContainer');
       sender.innerHTML = `<span>From:</span> ${email.sender}`;
       receiver.innerHTML =`<span>To:</span> ${email.recipients}`;
       subject.innerHTML =  `Subject: ${email.subject}`;
       body.innerHTML = email.body;
       timestamp.innerHTML = `<span>Timestamp:</span> ${email.timestamp}`;
-      divContainer.append(subject, sender, receiver,  body, timestamp, body);
+      divContainer.append(subject, sender, receiver, timestamp, btn, body);
       let viewEmail = document.querySelector('#view-email');
       viewEmail.innerHTML = '';
       viewEmail.append(divContainer);
       document.querySelector('#emails-view').style.display = 'none';
       viewEmail.style.display = 'block';
-   
+      btn.addEventListener('click', () => {
+        document.querySelector('#compose-recipients').disabled = true;
+        document.querySelector('#compose-subject').disabled = true;
+        let txt = ''
+        if (!(email.subject).startsWith('Re:'))
+        {
+          txt = 'Re: '
+        }
+        compose_email(email.recipients, txt+email.subject, `On ${email.timestamp} wrote:\n${email.body}`);
+      })
 }
